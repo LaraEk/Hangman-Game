cbdb651268e1de241db62885c368ac86ac68cf60
Ekscursionist <larakek@gmail.com>
2018-05-24 11:43:41 -0400

    continued work; adding assets; added arrays, some functions; stil have filler

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,48 @@
 $(document).ready(function() {
 
-    // Here are the arrays of names
+    // Here are the arrays of names.
     var elfWords = ["Galadriel","Arwen","Elendil","Elbereth","Legolas","Glorfindel"]
     var menWords = ["Aragorn","Denethor","Boromir","Faramir","Theoden","Eowyn"]
     var ardaWords = ["Eriador","Moria","Lothlorien","Gondor","Rohan","Mordor"]
 
-    // Here are the vars for 
+    // ~:~:~:~
+
+    // Here are the vars.  All the vars. 
+
+    var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+    var wins = 0;
+    var losses = 0;
+
+    // ~:~:~:~
+
+    // Here are the functions.  We function well.
+
+    function chooseElfWord() {
+        return elfWords[Math.floor(Math.random() * elfWords.length)];
+    }
+
+    function chooseMenWord() {
+        return menWords[Math.floor(Math.random() * menWords.length)];
+    }
+
+    function chooseArdaWord() {
+        return ardaWords[Math.floor(Math.random() * ardaWords.length)];
+    }
 
 
-
-
-      var pbjnum = 0;
-      var gcnum = 0;
-      var rbnum = 0;
-
-    // Here are the functions
-
-    // Here are the functions as they are called
+    // ~:~:~:~
+    
+    // Here are the functions as they are called.  Make It So.
 
     if
     elseif
     else
 
-    
+
+    wins++;
+    losses++;
+
 
     $("#pbj").on("click", function() {
       alert("Ate one more pbj!");
