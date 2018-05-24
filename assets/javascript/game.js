bfadb264d1a56b00d15290fa5d7a16d9f8916dcd
Ekscursionist <larakek@gmail.com>
2018-05-24 11:04:13 -0400

    beginning work on game; functions copypasta'd from sandich and to be replaced

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+    // Here are the arrays of names
+    var elfWords = ["Galadriel","Arwen","Elendil","Elbereth","Legolas","Glorfindel"]
+    var menWords = ["Aragorn","Denethor","Boromir","Faramir","Theoden","Eowyn"]
+    var ardaWords = ["Eriador","Moria","Lothlorien","Gondor","Rohan","Mordor"]
+
+    // Here are the vars for 
+
+
+
+
+      var pbjnum = 0;
+      var gcnum = 0;
+      var rbnum = 0;
+
+    // Here are the functions
+
+    // Here are the functions as they are called
+
+    if
+    elseif
+    else
+
+    
+
+    $("#pbj").on("click", function() {
+      alert("Ate one more pbj!");
+      pbjnum++;
+      $("#pbj").append ("you ate " + pbjnum + " pbjs!");
+      console.log(pbjnum);
+    });
+
+    $("#grilledcheese").on("click", function() {
+      alert("Ate one more grillcheese!");
+      gcnum++;
+      $(document.body.append ("you ate " + gcnum + " grillcheeses!"));
+      console.log(gcnum);
+    });
+
+    $("#roastbeef").on("click", function() {
+      alert("Ate one more beef!");
+      rbnum++;
+      $(document.body.append ("you ate " + rbnum + " beefs!"));
+      console.log(rbnum);
+    });
+
+
+    // ...
+
+  });
