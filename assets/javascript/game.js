005cbe38a7ba573c6d396acf2360fe6eabdffb5d
Ekscursionist <larakek@gmail.com>
2018-05-30 14:34:58 -0400

    let us at least save our continued frustrations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -71,15 +71,23 @@ $(document).ready(function() {
     { console.log(event.type);
 
         myguesses = myguesses + "," + event.key;
+        alert(myguesses + ", and you guessed " + event.key); // WHY DOES THIS WORK AND THE OTHER ONE DOESN'T??!?!?!
 
         if (letters.includes(event.key)) {
             console.log("yeah it's a letter");
             for(var z = 0; z <pickedword.length; z++) {
                 if (pickedword[z] === userGuess) {
-                pickedword[z] = userGuess;
-                $("#lettersguessed").append(userGuess);
-                guessesLeft = guessesLeft - 1;
-                console.log(guessesLeft);}            }                
+                    pickedword[z] = userGuess;
+                    $("#lettersguessed").append(userGuess);
+                    guessesLeft = guessesLeft - 1;
+                    console.log(guessesLeft);}            }   
+//                else{                                                 // wtf why is this an Unexpected Token Else everything is in curly brackets
+//                    console.log("that letter was not in the word");
+//                    alert("nope");
+//                    $("#lettersguessed").append(userGuess);
+//                    guessesLeft = guessesLeft - 1;
+//                    console.log(guessesLeft);}            
+                }              
             }
 
                 // THE CODE ABOVE IS SUPPOSED TO:
@@ -94,7 +102,7 @@ $(document).ready(function() {
         
         }
 
-        if(guessesLeft == 0)
+        if(guessesLeft == 0)   // I guess this needs to be inside a function?  But it's not even deincrementing anymore,so
         {
             alert('You Lost! Insert Balrog Here!');
             losses = losses + 1;
