b52547e46f7786edecd3897367188a46747d9f5b
Ekscursionist <larakek@gmail.com>
2018-05-31 21:57:15 -0400

    beginning to final-clean-up code so that it's clean and commented before submitting it as homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,10 +31,7 @@ $("#elfbutton").on("click", function() {
 // NOTE: THIS SECTION WORKS -------------- NOTE: THIS SECTION WORKS // NOTE: THIS SECTION WORKS -------------- NOTE: THIS SECTION WORKS 
 
 
-// -------------------------- NEXT SECTION ---------------------- //
-
-
-// NOTE: Here are the vars and the arrays of names.  NOTE: All lowercase so as to lessen confusion.
+// Here are the vars and the arrays of names.  NOTE: All lowercase so as to lessen confusion.
 var elfWords = ["galadriel","arwen","elendil","elbereth","legolas","glorfindel"];
 var menWords = ["aragorn","denethor","boromir","faramir","theoden","eowyn"];
 var ardaWords = ["eriador","moria","lothlorien","gondor","rohan","mordor"];
@@ -45,91 +42,98 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var myguesses = "";
+// Vars and arrays of names end. ------------------------------------------------------------ //
 
 
-
+// Main document ready function ------------------------------------------------------------ //
 $(document).ready(function() {
 
-    console.log("please console log something already");
+    console.log("the game begins");
 
 
     $("#buttons").on("click", "#elfbutton", function(event) {
 
+// --------------- This picks a random word ('pickaword' ) and stores its underscore array in 'pickedword' ---------- //      
         var pickaword = elfWords[Math.floor(Math.random() * elfWords.length)];
         console.log(pickaword);
         var pickedword = [];
 
-    for(var i = 0; i < pickaword.length; i++){
-        pickedword[i] = "_ ";
-    }
-    $("#wordispicked").html(pickedword.join(' '));
-
-    console.log("console log this when this section works");
-    console.log(pickedword);
-
-    document.onkeypress = function(event)
-    { console.log(event.type);
-        var userGuess = event.key;
-         var found = false;
-        myguesses = myguesses + "," + event.key;
-        //alert(myguesses + ", and you guessed " + event.key); // WHY DOES THIS WORK AND THE OTHER ONE DOESN'T??!?!?!
-
-        if (letters.includes(event.key)) {
-            console.log("yeah it's a letter");
-            for(var z = 0; z <pickaword.length; z++) {
-                if (pickaword[z] === userGuess) {
-                    found = true;
-                    pickedword[z] = userGuess;
-                    document.querySelector("#wordispicked").innerHTML = pickedword.join(' ');
-
-                } 
-            } // to the FOR   
-        } else {
-                console.log("that ain't no letter");
-                alert("That is not, in fact, a letter.");
-                }
-            
-            
-
-               if(found == false)
-               {
-                    document.querySelector("#lettersguessed").innerHTML = myguesses;
-//                          $("#lettersguessed").html(myguesses);
-                    alert("that letter was not in the word");
-
-                    console.log(guessesLeft);
-                    console.log("nope");
-                    guessesLeft = guessesLeft - 1;
-                    document.querySelector("#guessesleft").innerHTML = guessesLeft;
-//                          $("#guessesleft").html(guessesLeft);
-                    console.log(guessesLeft);                   
-
-                } // to the Z PICKAWORD IF/ELSE
-
-
-            if(guessesLeft == 0)   // I guess this needs to be inside a function?  But it's not even deincrementing anymore,so
-            {
-                alert('You Lost! Insert Balrog Here!');
-                losses = losses + 1;
-                document.querySelector("#lossesdiv").innerHTML = losses;            
-                resetGame();
-            }    
-
-            // if(pickedword.indexOf("_") === -1) {
-            //     wins = wins + 1;
-            //     document.querySelector("#winsdiv").innerHTML = wins;            
-            //     alert('You Lost! Insert Balrog Here!');
-            //     resetGame();
-            // }
-
-            function resetGame() {
-                document.querySelector("#winsdiv").innerHTML = wins;
-                document.querySelector("#lossesdiv").innerHTML = losses;            
-                guessesLeft = 10
-                myguesses = "";
-                alert("To start a new game, choose Elves, Men, or Arda.")
-            }
+        for(var i = 0; i < pickaword.length; i++){
+            pickedword[i] = "_ ";
         }
+
+        $("#wordispicked").html(pickedword.join(' '));
+
+        console.log("console log this when this section works");
+        console.log(pickedword);
+// --------------- This picks a random word ('pickaword' ) and stores its underscore array in 'pickedword' ---------- //      
+
+
+
+// --------------- This is the part where the player chooses a letter and it gets put in the right underscore place.  ---------- //      
+        document.onkeypress = function(event)
+        { console.log(event.type);
+            var userGuess = event.key;
+            var correctLetter = false;
+            myguesses = myguesses + "," + event.key;
+
+            if (letters.includes(event.key)) {
+                console.log("yeah it's a letter");
+                for(var z = 0; z <pickaword.length; z++) {
+                    if (pickaword[z] === userGuess) {
+                        correctLetter = true;
+                        pickedword[z] = userGuess;
+                        document.querySelector("#wordispicked").innerHTML = pickedword.join(' ');
+
+                    } 
+                } // to the FOR   
+            } else {
+                    console.log("that ain't no letter");
+                    alert("That is not, in fact, a letter.");
+                    }
+                
+                
+
+                if(correctLetter == false)
+                {
+                        document.querySelector("#lettersguessed").innerHTML = myguesses;
+    //                          $("#lettersguessed").html(myguesses);
+                        alert("that letter was not in the word");
+
+                        console.log(guessesLeft);
+                        console.log("nope");
+                        guessesLeft = guessesLeft - 1;
+                        document.querySelector("#guessesleft").innerHTML = guessesLeft;
+    //                          $("#guessesleft").html(guessesLeft);
+                        console.log(guessesLeft);                   
+
+                    } // to the Z PICKAWORD IF/ELSE
+
+
+                if(guessesLeft == 0)   // I guess this needs to be inside a function?  But it's not even deincrementing anymore,so
+                {
+                    alert('You Lost! Insert Balrog Here!');
+                    losses = losses + 1;
+                    document.querySelector("#lossesdiv").innerHTML = losses;            
+                    resetGame();
+                }    
+
+                // if(pickedword.indexOf("_") === -1) {
+                //     wins = wins + 1;
+                //     document.querySelector("#winsdiv").innerHTML = wins;            
+                //     alert('You Lost! Insert Balrog Here!');
+                //     resetGame();
+                // }
+
+                function resetGame() {
+                    document.querySelector("#winsdiv").innerHTML = wins;
+                    document.querySelector("#lossesdiv").innerHTML = losses;            
+                    guessesLeft = 10
+                    myguesses = "";
+                    alert("To start a new game, choose Elves, Men, or Arda.")
+                }
+            }
+// --------------- This is the end of the part where the player chooses a letter and it gets put in the right underscore place.  ---------- //      
 
     var winsGame= document.getElementById("winsdiv");
     winsGame.innerHTML = wins;
@@ -147,6 +151,8 @@ $(document).ready(function() {
     
 
 });  // @@@ this is for document ready function @@@
+// Finish Main document ready function ------------------------------------------------------------ //
+
 
     // The End! \o/
     // ~:~:~:~
