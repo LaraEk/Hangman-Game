4a5e5f01cb074b110d15eb66fa08bac54add1590
Ekscursionist <larakek@gmail.com>
2018-05-31 23:05:55 -0400

    FINALLYgit add .! FINISHEDgit add .!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -48,8 +48,13 @@ var myguesses = "";
 // Main document ready function ------------------------------------------------------------ //
 $(document).ready(function() {
 
+    alert("To begin the game, please click on Elves, Men, or Arda.")
     console.log("the game begins");
 
+
+// -------------------------------------- ELVES SECTION -------------------------------------- //
+// -------------------------------------- ELVES SECTION -------------------------------------- //
+// -------------------------------------- ELVES SECTION -------------------------------------- //
 
     $("#buttons").on("click", "#elfbutton", function(event) {
 
@@ -77,6 +82,7 @@ $(document).ready(function() {
             var correctLetter = false;
             myguesses = myguesses + "," + event.key;
 
+// ---------------------- This is what happens if the chosen letter is in the picked word -----------------------//
             if (letters.includes(event.key)) {
                 console.log("yeah it's a letter");
                 for(var z = 0; z <pickaword.length; z++) {
@@ -84,47 +90,47 @@ $(document).ready(function() {
                         correctLetter = true;
                         pickedword[z] = userGuess;
                         document.querySelector("#wordispicked").innerHTML = pickedword.join(' ');
-
-                    } 
+                    } // to the if-pickaword-z 
                 } // to the FOR   
             } else {
                     console.log("that ain't no letter");
                     alert("That is not, in fact, a letter.");
                     }
+// ---------------------- This is what happens if the chosen letter is in the picked word -----------------------//
                 
                 
-
-                if(correctLetter == false)
-                {
+// ---------------------- This is what happens if the chosen letter is NOT in the picked word -----------------------//
+                if(correctLetter == false) {
                         document.querySelector("#lettersguessed").innerHTML = myguesses;
-    //                          $("#lettersguessed").html(myguesses);
                         alert("that letter was not in the word");
-
                         console.log(guessesLeft);
                         console.log("nope");
                         guessesLeft = guessesLeft - 1;
                         document.querySelector("#guessesleft").innerHTML = guessesLeft;
-    //                          $("#guessesleft").html(guessesLeft);
                         console.log(guessesLeft);                   
-
                     } // to the Z PICKAWORD IF/ELSE
+// ---------------------- This is what happens if the chosen letter is NOT in the picked word -----------------------//
 
 
-                if(guessesLeft == 0)   // I guess this needs to be inside a function?  But it's not even deincrementing anymore,so
-                {
+// ---------------------- This is the Loss Condition ----------------------//
+                if(guessesLeft == 0)       {
                     alert('You Lost! Insert Balrog Here!');
                     losses = losses + 1;
                     document.querySelector("#lossesdiv").innerHTML = losses;            
                     resetGame();
                 }    
+// ---------------------- This is the Loss Condition ----------------------//
 
-                // if(pickedword.indexOf("_") === -1) {
-                //     wins = wins + 1;
-                //     document.querySelector("#winsdiv").innerHTML = wins;            
-                //     alert('You Lost! Insert Balrog Here!');
-                //     resetGame();
-                // }
+// ---------------------- This is the Win Condition -----------------------//
+                if(pickedword.indexOf("_ ") === -1) {
+                    wins = wins + 1;
+                    document.querySelector("#winsdiv").innerHTML = wins;            
+                    alert('You Win! Gandalf would be so proud!');
+                    resetGame();
+                }
+// ---------------------- This is the Win Condition -----------------------//
 
+// ------------------- This is the Reset Game Condition -------------------//
                 function resetGame() {
                     document.querySelector("#winsdiv").innerHTML = wins;
                     document.querySelector("#lossesdiv").innerHTML = losses;            
@@ -133,6 +139,8 @@ $(document).ready(function() {
                     alert("To start a new game, choose Elves, Men, or Arda.")
                 }
             }
+// ------------------- This is the Reset Game Condition -------------------//
+
 // --------------- This is the end of the part where the player chooses a letter and it gets put in the right underscore place.  ---------- //      
 
     var winsGame= document.getElementById("winsdiv");
@@ -148,7 +156,230 @@ $(document).ready(function() {
     showGuesses.innerHTML = myguesses;
 
     }); // @@@ this is for buttons on click elfbutton @@@
+// -------------------------------------- FINISH ELVES SECTION -------------------------------------- //
+// -------------------------------------- FINISH ELVES SECTION -------------------------------------- //
+// -------------------------------------- FINISH ELVES SECTION -------------------------------------- //
     
+
+
+// -------------------------------------- MEN SECTION -------------------------------------- //
+// -------------------------------------- MEN SECTION -------------------------------------- //
+// -------------------------------------- MEN SECTION -------------------------------------- //
+
+    $("#buttons").on("click", "#menbutton", function(event) {
+
+// --------------- This picks a random word ('pickaword' ) and stores its underscore array in 'pickedword' ---------- //      
+        var pickaword = menWords[Math.floor(Math.random() * menWords.length)];
+        console.log(pickaword);
+        var pickedword = [];
+
+        for(var i = 0; i < pickaword.length; i++){
+            pickedword[i] = "_ ";
+        }
+
+        $("#wordispicked").html(pickedword.join(' '));
+
+        console.log("console log this when this section works");
+        console.log(pickedword);
+// --------------- This picks a random word ('pickaword' ) and stores its underscore array in 'pickedword' ---------- //      
+
+
+
+// --------------- This is the part where the player chooses a letter and it gets put in the right underscore place.  ---------- //      
+        document.onkeypress = function(event)
+        { console.log(event.type);
+            var userGuess = event.key;
+            var correctLetter = false;
+            myguesses = myguesses + "," + event.key;
+
+// ---------------------- This is what happens if the chosen letter is in the picked word -----------------------//
+            if (letters.includes(event.key)) {
+                console.log("yeah it's a letter");
+                for(var z = 0; z <pickaword.length; z++) {
+                    if (pickaword[z] === userGuess) {
+                        correctLetter = true;
+                        pickedword[z] = userGuess;
+                        document.querySelector("#wordispicked").innerHTML = pickedword.join(' ');
+                    } // to the if-pickaword-z 
+                } // to the FOR   
+            } else {
+                    console.log("that ain't no letter");
+                    alert("That is not, in fact, a letter.");
+                    }
+// ---------------------- This is what happens if the chosen letter is in the picked word -----------------------//
+                
+                
+// ---------------------- This is what happens if the chosen letter is NOT in the picked word -----------------------//
+                if(correctLetter == false) {
+                        document.querySelector("#lettersguessed").innerHTML = myguesses;
+                        alert("that letter was not in the word");
+                        console.log(guessesLeft);
+                        console.log("nope");
+                        guessesLeft = guessesLeft - 1;
+                        document.querySelector("#guessesleft").innerHTML = guessesLeft;
+                        console.log(guessesLeft);                   
+                    } // to the Z PICKAWORD IF/ELSE
+// ---------------------- This is what happens if the chosen letter is NOT in the picked word -----------------------//
+
+
+// ---------------------- This is the Loss Condition ----------------------//
+                if(guessesLeft == 0)       {
+                    alert('You Lost! Insert Balrog Here!');
+                    losses = losses + 1;
+                    document.querySelector("#lossesdiv").innerHTML = losses;            
+                    resetGame();
+                }    
+// ---------------------- This is the Loss Condition ----------------------//
+
+// ---------------------- This is the Win Condition -----------------------//
+                if(pickedword.indexOf("_ ") === -1) {
+                    wins = wins + 1;
+                    document.querySelector("#winsdiv").innerHTML = wins;            
+                    alert('You Win! Gandalf would be so proud!');
+                    resetGame();
+                }
+// ---------------------- This is the Win Condition -----------------------//
+
+// ------------------- This is the Reset Game Condition -------------------//
+                function resetGame() {
+                    document.querySelector("#winsdiv").innerHTML = wins;
+                    document.querySelector("#lossesdiv").innerHTML = losses;            
+                    guessesLeft = 10
+                    myguesses = "";
+                    alert("To start a new game, choose Elves, Men, or Arda.")
+                }
+            }
+// ------------------- This is the Reset Game Condition -------------------//
+
+// --------------- This is the end of the part where the player chooses a letter and it gets put in the right underscore place.  ---------- //      
+
+    var winsGame= document.getElementById("winsdiv");
+    winsGame.innerHTML = wins;
+
+    var lossesGame= document.getElementById("lossesdiv");
+    lossesGame.innerHTML = losses;
+    
+    var numberOfGuessesLeft = document.getElementById("guessesleft"); // guessesLeft = #guessesleft
+    numberOfGuessesLeft.innerHTML = guessesLeft;
+    
+    var showGuesses = document.getElementById("lettersguessed"); // #lettersguessed = myguesses
+    showGuesses.innerHTML = myguesses;
+
+    }); // @@@ this is for buttons on click menbutton @@@
+// -------------------------------------- FINISH MEN SECTION -------------------------------------- //
+// -------------------------------------- FINISH MEN SECTION -------------------------------------- //
+// -------------------------------------- FINISH MEN SECTION -------------------------------------- //
+    
+
+
+// -------------------------------------- ARDA SECTION -------------------------------------- //
+// -------------------------------------- ARDA SECTION -------------------------------------- //
+// -------------------------------------- ARDA SECTION -------------------------------------- //
+
+        $("#buttons").on("click", "#ardabutton", function(event) {
+
+// --------------- This picks a random word ('pickaword' ) and stores its underscore array in 'pickedword' ---------- //      
+        var pickaword = ardaWords[Math.floor(Math.random() * ardaWords.length)];
+        console.log(pickaword);
+        var pickedword = [];
+
+        for(var i = 0; i < pickaword.length; i++){
+            pickedword[i] = "_ ";
+        }
+
+        $("#wordispicked").html(pickedword.join(' '));
+
+        console.log("console log this when this section works");
+        console.log(pickedword);
+// --------------- This picks a random word ('pickaword' ) and stores its underscore array in 'pickedword' ---------- //      
+
+
+
+// --------------- This is the part where the player chooses a letter and it gets put in the right underscore place.  ---------- //      
+        document.onkeypress = function(event)
+        { console.log(event.type);
+            var userGuess = event.key;
+            var correctLetter = false;
+            myguesses = myguesses + "," + event.key;
+
+// ---------------------- This is what happens if the chosen letter is in the picked word -----------------------//
+            if (letters.includes(event.key)) {
+                console.log("yeah it's a letter");
+                for(var z = 0; z <pickaword.length; z++) {
+                    if (pickaword[z] === userGuess) {
+                        correctLetter = true;
+                        pickedword[z] = userGuess;
+                        document.querySelector("#wordispicked").innerHTML = pickedword.join(' ');
+                    } // to the if-pickaword-z 
+                } // to the FOR   
+            } else {
+                    console.log("that ain't no letter");
+                    alert("That is not, in fact, a letter.");
+                    }
+// ---------------------- This is what happens if the chosen letter is in the picked word -----------------------//
+                
+                
+// ---------------------- This is what happens if the chosen letter is NOT in the picked word -----------------------//
+                if(correctLetter == false) {
+                        document.querySelector("#lettersguessed").innerHTML = myguesses;
+                        alert("that letter was not in the word");
+                        console.log(guessesLeft);
+                        console.log("nope");
+                        guessesLeft = guessesLeft - 1;
+                        document.querySelector("#guessesleft").innerHTML = guessesLeft;
+                        console.log(guessesLeft);                   
+                    } // to the Z PICKAWORD IF/ELSE
+// ---------------------- This is what happens if the chosen letter is NOT in the picked word -----------------------//
+
+
+// ---------------------- This is the Loss Condition ----------------------//
+                if(guessesLeft == 0)       {
+                    alert('You Lost! Insert Balrog Here!');
+                    losses = losses + 1;
+                    document.querySelector("#lossesdiv").innerHTML = losses;            
+                    resetGame();
+                }    
+// ---------------------- This is the Loss Condition ----------------------//
+
+// ---------------------- This is the Win Condition -----------------------//
+                if(pickedword.indexOf("_ ") === -1) {
+                    wins = wins + 1;
+                    document.querySelector("#winsdiv").innerHTML = wins;            
+                    alert('You Win! Gandalf would be so proud!');
+                    resetGame();
+                }
+// ---------------------- This is the Win Condition -----------------------//
+
+// ------------------- This is the Reset Game Condition -------------------//
+                function resetGame() {
+                    document.querySelector("#winsdiv").innerHTML = wins;
+                    document.querySelector("#lossesdiv").innerHTML = losses;            
+                    guessesLeft = 10
+                    myguesses = "";
+                    alert("To start a new game, choose Elves, Men, or Arda.")
+                }
+            }
+// ------------------- This is the Reset Game Condition -------------------//
+
+// --------------- This is the end of the part where the player chooses a letter and it gets put in the right underscore place.  ---------- //      
+
+    var winsGame= document.getElementById("winsdiv");
+    winsGame.innerHTML = wins;
+
+    var lossesGame= document.getElementById("lossesdiv");
+    lossesGame.innerHTML = losses;
+    
+    var numberOfGuessesLeft = document.getElementById("guessesleft"); // guessesLeft = #guessesleft
+    numberOfGuessesLeft.innerHTML = guessesLeft;
+    
+    var showGuesses = document.getElementById("lettersguessed"); // #lettersguessed = myguesses
+    showGuesses.innerHTML = myguesses;
+
+    }); // @@@ this is for buttons on click ardabutton @@@
+// -------------------------------------- FINISH ARDA SECTION -------------------------------------- //
+// -------------------------------------- FINISH ARDA SECTION -------------------------------------- //
+// -------------------------------------- FINISH ARDA SECTION -------------------------------------- //
+
 
 });  // @@@ this is for document ready function @@@
 // Finish Main document ready function ------------------------------------------------------------ //
